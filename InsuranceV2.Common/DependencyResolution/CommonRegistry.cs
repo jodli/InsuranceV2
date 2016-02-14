8b65179829efae071b364f71c0cde2fb37174ecd
jodli <job87@web.de>
2016-02-14 17:28:01 +0100

    Added color to debug console logging.

--- InsuranceV2.Common/DependencyResolution/CommonRegistry.cs
+++ InsuranceV2.Common/DependencyResolution/CommonRegistry.cs
@@ -17,6 +17,17 @@ namespace InsuranceV2.Common.DependencyResolution
 
             For(typeof (ILogger<>)).Use(typeof (Logger<>));
             XmlConfigurator.Configure();
+
+            TestLogging();
+        }
+
+        private static void TestLogging()
+        {
+            var logger = new Logger<CommonRegistry>();
+            logger.Debug("Testing Debug Log.");
+            logger.Info("Testing Info Log.");
+            logger.Warn("Testing Warn Log.");
+            logger.Error("Testing Error Log.");
         }
     }
 }
