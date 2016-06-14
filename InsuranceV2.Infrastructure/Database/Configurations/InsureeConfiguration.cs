da22d04d96b1b598a1ab8a5c323e9e6ad0b489aa
jodli <job87@web.de>
2016-06-14 11:17:50 +0200

    Changed database dates to type: date.

--- InsuranceV2.Infrastructure/Database/Configurations/InsureeConfiguration.cs
+++ InsuranceV2.Infrastructure/Database/Configurations/InsureeConfiguration.cs
@@ -9,6 +9,13 @@ namespace InsuranceV2.Infrastructure.Database.Configurations
         {
             Property(x => x.FirstName).IsRequired().HasMaxLength(50);
             Property(x => x.LastName).IsRequired().HasMaxLength(50);
+
+            Property(x => x.DateOfBirth).HasColumnType("date");
+
+            Property(x => x.DateOfMarriage).HasColumnType("date");
+
+            Property(x => x.DateCreated).IsRequired().HasColumnType("date");
+            Property(x => x.DateModified).IsRequired().HasColumnType("date");
         }
     }
 }
