0da2ba4c91d3df13e8c4cff3202ac5b1997e37bf
jodli <job87@web.de>
2016-08-04 12:57:57 +0200

    Added Startup View. Updated Todo list. Changed startup size of shell.

---
+++ Source/InsuranceV2.Modules/Content/Views/StartupView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace InsuranceV2.Modules.Content.Views
+{
+    /// <summary>
+    /// Interaction logic for StartupView
+    /// </summary>
+    public partial class StartupView : UserControl
+    {
+        public StartupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
